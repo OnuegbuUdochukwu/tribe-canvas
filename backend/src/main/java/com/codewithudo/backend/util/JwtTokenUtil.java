fe55a0c1876741417a2b6068d572145ed9352347
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-08-15 12:09:21 +0100

    feat: Added JWTokenUtil

---
+++ backend/src/main/java/com/codewithudo/backend/util/JwtTokenUtil.java
@@ -0,0 +1,41 @@
+package com.codewithudo.backend.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenUtil {
+
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60; // 5 hours
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    // Retrieve secret key
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // Generate token for user
+    public String generateToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, email);
+    }
+
+    // While creating the token
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+    }
+}
