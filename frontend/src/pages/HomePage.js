b39b16155a720916692145460389ab80a078b742
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-08-26 21:26:11 +0100

    fix: Added Homepage

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Welcome to Tribe Canvas</h2>
+      <p>Discover and purchase beautiful art from Nigerian artists.</p>
+    </div>
+  );
+};
+
+export default HomePage;
