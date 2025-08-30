77c8335035a545a0679c5e00a5a5c68b1c570532
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-08-30 09:25:08 +0100

    fix(navbar): correct JSX and add Order History link for authenticated users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,65 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './AuthContext';
-import { CartProvider } from './CartContext';
-import NavBar from './components/NavBar';
-import PrivateRoute from './components/PrivateRoute';
-import './index.css';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage'; // New import
-import OrderHistoryPage from './pages/OrderHistoryPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
+import { CartProvider } from "./CartContext";
+import NavBar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
+import "./index.css";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage"; // New import
+import OrderHistoryPage from "./pages/OrderHistoryPage";
 
 // Placeholder pages
-import HomePage from './pages/HomePage';
-import GalleryPage from './pages/GalleryPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ArtistDashboard from './pages/ArtistDashboard';
+import HomePage from "./pages/HomePage";
+import GalleryPage from "./pages/GalleryPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ArtistDashboard from "./pages/ArtistDashboard";
 const ProductPage = () => <div>Product Page</div>;
 
 function App() {
-  return (
-    <AuthProvider>
-      <CartProvider>
-        <div className="App">
-          <NavBar />
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/gallery" element={<GalleryPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/checkout" element={<CheckoutPage />} /> {/* New route */}
-              <Route path="/orders" element={<OrderHistoryPage />} />
-              <Route path="/dashboard" element={
-                <PrivateRoute>
-                  <ArtistDashboard />
-                </PrivateRoute>
-              } />
-              <Route path="/artworks/:id" element={<ProductPage />} />
-            </Routes>
-          </main>
-        </div>
-      </CartProvider>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <CartProvider>
+                <div className="App">
+                    <NavBar />
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/gallery" element={<GalleryPage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route
+                                path="/register"
+                                element={<RegisterPage />}
+                            />
+                            <Route path="/cart" element={<CartPage />} />
+                            <Route
+                                path="/checkout"
+                                element={<CheckoutPage />}
+                            />{" "}
+                            {/* New route */}
+                            <Route
+                                path="/orders"
+                                element={<OrderHistoryPage />}
+                            />
+                            <Route
+                                path="/dashboard"
+                                element={
+                                    <PrivateRoute>
+                                        <ArtistDashboard />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route
+                                path="/artworks/:id"
+                                element={<ProductPage />}
+                            />
+                        </Routes>
+                    </main>
+                </div>
+            </CartProvider>
+        </AuthProvider>
+    );
 }
 
 export default App;
